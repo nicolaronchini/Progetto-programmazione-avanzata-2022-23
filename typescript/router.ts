297a88dde927acd6f1e5965cef9f126429c78593
nicolaronchini <s1113274@studenti.univpm.it>
2023-07-03 15:53:35 +0200

    jwt testing

--- typescript/router.ts
+++ typescript/router.ts
@@ -1,25 +1,18 @@
 var express = require('express');
 import * as controller from './controller'
+import * as Middleware from './middleware/autorizzazioneJWT'
+const jwt = require('jsonwebtoken');
 
 const app = express();
 
 app.use(express.json());
 
-/*
-app.get('/test1',(req,res) => {
-    let result = controller.test('adrianomancinifalso@gmail.com');
-    if (result === null) {res.send("La mail non esiste")}
-    else {res.send("La mail compare nel database")}
-});
-*/
-
-app.get('/test',(req,res) => {
-    res.send("prova");
+app.get('/test',Middleware.JWT,(req,res) => {
+    res.send(req.token)
 });
 
-app.post('/crea-segnalazione',(req,res) => {
-    const tipo = req.body.tipo;
-    controller.creaSegnalazioni(tipo);
+app.post('/crea-segnalazione',Middleware.JWT,(req,res) => {
+    controller.creaSegnalazioni(req);
     res.send("success");
 });
 
